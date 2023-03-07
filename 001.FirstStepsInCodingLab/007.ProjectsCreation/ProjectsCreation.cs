1146a119e295218515cef2457feb0d3d65519873
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-07 14:03:37 +0200

    add

---
+++ 001.FirstStepsInCodingLab/007.ProjectsCreation/ProjectsCreation.cs
@@ -0,0 +1,16 @@
+﻿//First steps in coding - lab, Task 007
+//07.03.2023, 13:32
+using System;
+
+public class ProjectsCreation
+{
+    static void Main()
+    {
+        string name = Console.ReadLine();
+        int projects = int.Parse(Console.ReadLine());
+
+        int neededHours = projects * 3;
+
+        Console.WriteLine($"The architect {name} will need {neededHours} hours to complete {projects} project/s.");
+    }
+}
