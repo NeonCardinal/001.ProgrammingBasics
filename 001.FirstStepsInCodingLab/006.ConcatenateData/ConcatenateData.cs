514ef62bb3e739b466e16aa32f8310666471b7a6
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-07 14:03:19 +0200

    add

---
+++ 001.FirstStepsInCodingLab/006.ConcatenateData/ConcatenateData.cs
@@ -0,0 +1,16 @@
+﻿//First steps in coding - lab, Task 006
+//07.03.2023, 13:28
+using System;
+
+public class ConcatenateData
+{
+    static void Main()
+    {
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+        string town = Console.ReadLine();
+
+        Console.WriteLine($"You are {firstName} {lastName}, a {age}-years old person from {town}.");
+    }
+}
