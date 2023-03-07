7cdcc36d430fc089d31fbeafe887d89741844875
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-07 14:04:09 +0200

    add

---
+++ 001.FirstStepsInCodingLab/009.YardGreening/YardGreening.cs
@@ -0,0 +1,18 @@
+﻿//First steps in coding - lab, Task 009
+//07.03.2023, 13:39
+using System;
+
+public class YardGreening
+{
+    static void Main()
+    {
+        double area = double.Parse(Console.ReadLine());
+
+        double price = area * 7.61;
+        double discount = price * 0.18;
+        double total = price - discount;
+
+        Console.WriteLine($"The final price is: {total} lv.");
+        Console.WriteLine($"The discount is: {discount} lv.");
+    }
+}
