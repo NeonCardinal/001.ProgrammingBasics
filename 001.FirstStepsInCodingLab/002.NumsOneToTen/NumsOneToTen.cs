3fc63547cbcf76f35421c785f09e27e27cfad61d
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-07 14:02:11 +0200

    add

---
+++ 001.FirstStepsInCodingLab/002.NumsOneToTen/NumsOneToTen.cs
@@ -0,0 +1,20 @@
+﻿//First steps in coding - lab, Task 002
+//07.03.2023, 13:20
+using System;
+
+public class NumsOneToTen
+{
+    static void Main()
+    {
+        Console.WriteLine(1);
+        Console.WriteLine(2);
+        Console.WriteLine(3);
+        Console.WriteLine(4);
+        Console.WriteLine(5);
+        Console.WriteLine(6);
+        Console.WriteLine(7);
+        Console.WriteLine(8);
+        Console.WriteLine(9);
+        Console.WriteLine(10);
+    }
+}
