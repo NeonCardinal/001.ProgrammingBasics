825d3bdd02708388d751942bbcda0a291c85893e
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-07 14:03:02 +0200

    add

---
+++ 001.FirstStepsInCodingLab/005.GreetingByName/GreetingByName.cs
@@ -0,0 +1,13 @@
+﻿//First steps in coding - lab, Task 005
+//07.03.2023, 13:26
+using System;
+
+public class GreetingByName
+{
+    static void Main()
+    {
+        string name = Console.ReadLine();
+
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
