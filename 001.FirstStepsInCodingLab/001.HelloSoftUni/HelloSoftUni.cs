2914efee2d5f2cda043ebee9f21778b87adad974
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-07 14:01:41 +0200

    add

---
+++ 001.FirstStepsInCodingLab/001.HelloSoftUni/HelloSoftUni.cs
@@ -0,0 +1,11 @@
+﻿//First steps in coding - lab, Task 001
+//07.03.2023, 13:17
+using System;
+
+public class HelloSoftUni
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello SoftUni");
+    }
+}
