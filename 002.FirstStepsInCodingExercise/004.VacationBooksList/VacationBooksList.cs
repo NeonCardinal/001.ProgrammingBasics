7407588c917811feb4d1e87bb487422862aaa1df
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-08 09:45:08 +0200

    add

---
+++ 002.FirstStepsInCodingExercise/004.VacationBooksList/VacationBooksList.cs
@@ -0,0 +1,18 @@
+﻿//First steps in coding - exercise, Task 004
+//07.03.2023, 14:27
+using System;
+
+public class VacationBooksList
+{
+    static void Main()
+    {
+        int pagesCount = int.Parse(Console.ReadLine());
+        int pagesPerHour = int.Parse(Console.ReadLine());
+        int days = int.Parse(Console.ReadLine());
+
+        int totalTimePerBook = pagesCount / pagesPerHour;
+        int neededHours = totalTimePerBook / days;
+
+        Console.WriteLine(neededHours);
+    }
+}
