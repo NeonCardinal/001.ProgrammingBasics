b43517b37e4797ef847a5cd420fed51c132ad72d
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-08 09:46:02 +0200

    add

---
+++ 002.FirstStepsInCodingExercise/007.FoodDelivery/FoodDelivery.cs
@@ -0,0 +1,22 @@
+﻿//First steps in coding - exercise, Task 007
+//07.03.2023, 14:48
+using System;
+
+public class FoodDelivery
+{
+    static void Main()
+    {
+        int chicken = int.Parse(Console.ReadLine());
+        int fish = int.Parse(Console.ReadLine());
+        int vegetable = int.Parse(Console.ReadLine());
+
+        double chickenPrice = chicken * 10.35;
+        double fishPrice = fish * 12.40;
+        double vegetablePrice = vegetable * 8.15;
+        double foodPrice = chickenPrice + fishPrice + vegetablePrice;
+        double desertPrice = foodPrice * 0.20;
+        double total = foodPrice + desertPrice + 2.50;
+
+        Console.WriteLine(total);
+    }
+}
