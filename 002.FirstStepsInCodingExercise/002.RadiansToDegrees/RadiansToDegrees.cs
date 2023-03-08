57e3c4e3f76669595fbfbb1e5e1c04520f99ead4
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-08 09:44:31 +0200

    add

---
+++ 002.FirstStepsInCodingExercise/002.RadiansToDegrees/RadiansToDegrees.cs
@@ -0,0 +1,15 @@
+﻿//First steps in coding - exercise, Task 002
+//07.03.2023, 14:19
+using System;
+
+public class RadiansToDegrees
+{
+    static void Main()
+    {
+        double rad = double.Parse(Console.ReadLine());
+
+        double deg = rad * 180 / Math.PI;
+
+        Console.WriteLine(deg);
+    }
+}
