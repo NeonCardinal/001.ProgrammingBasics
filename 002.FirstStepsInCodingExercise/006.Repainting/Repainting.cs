c58877e8f397c96bb675b0e069b3a015cb4706dd
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-08 09:45:43 +0200

    add

---
+++ 002.FirstStepsInCodingExercise/006.Repainting/Repainting.cs
@@ -0,0 +1,24 @@
+﻿//First steps in coding - exercise, Task 006
+//07.03.2023, 14:41
+using System;
+
+public class Repainting
+{
+    static void Main()
+    {
+        int nylon = int.Parse(Console.ReadLine());
+        int dye = int.Parse(Console.ReadLine());
+        int litters = int.Parse(Console.ReadLine());
+        int hours = int.Parse(Console.ReadLine());
+
+        double nylonPrice = (nylon + 2) * 1.50;
+        double extraDye = dye * 0.10;
+        double dyePrice = (dye + extraDye) * 14.50;
+        double litterPrice = litters * 5.00;
+        double priceForAll = nylonPrice + dyePrice + litterPrice + 0.40;
+        double workersPrice = priceForAll * 0.30;
+        double total = priceForAll + (workersPrice * hours);
+
+        Console.WriteLine(total);
+    }
+}
