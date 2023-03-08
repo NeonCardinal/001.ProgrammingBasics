9141e61bd505ecead672375208ffc480ba9cb2d0
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-08 09:44:50 +0200

    add

---
+++ 002.FirstStepsInCodingExercise/003.DepositCalculator/DepositCalculator.cs
@@ -0,0 +1,19 @@
+﻿//First steps in coding - exercise, Task 003
+//07.03.2023, 14:22
+using System;
+
+public class DepositCalculator
+{
+    static void Main()
+    {
+        double depositAmount = double.Parse(Console.ReadLine());
+        int depositPeriod = int.Parse(Console.ReadLine());
+        double yearAdd = double.Parse(Console.ReadLine());
+
+        double add = depositAmount * (yearAdd / 100.0);
+        double addPerMonth = add / 12;
+        double total = depositAmount + depositPeriod * addPerMonth;
+
+        Console.WriteLine(total);
+    }
+}
