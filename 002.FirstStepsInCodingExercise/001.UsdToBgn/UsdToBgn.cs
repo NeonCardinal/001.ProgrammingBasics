0ca2d91c58eb46ad8f7d3db6416e289cd142e719
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-08 09:44:14 +0200

    add

---
+++ 002.FirstStepsInCodingExercise/001.UsdToBgn/UsdToBgn.cs
@@ -0,0 +1,15 @@
+﻿//Fist steps in coding - exercise, Task 001
+//07.03.2023, 14:17
+using System;
+
+public class UsdToBgn
+{
+    static void Main()
+    {
+        double usd = double.Parse(Console.ReadLine());
+
+        double bgn = usd * 1.79549;
+
+        Console.WriteLine(bgn);
+    }
+}
