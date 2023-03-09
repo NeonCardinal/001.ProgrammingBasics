273613e1e0b0cf657c99754c9e5ea97efa24098b
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-09 10:07:02 +0200

    add

---
+++ 003.ConditionalStatementsLab/001.ExcellentResult/ExcellentResult.cs
@@ -0,0 +1,16 @@
+﻿//Conditional statements - lab, Task 001
+//07.03.2023, 15:30
+using System;
+
+public class ExcellentResult
+{
+    static void Main()
+    {
+        double grade = double.Parse(Console.ReadLine());
+
+        if(grade >= 5.50)
+        {
+            Console.WriteLine("Excellent!");
+        }
+    }
+}
