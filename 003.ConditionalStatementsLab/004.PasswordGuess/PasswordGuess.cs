69cc75f4fcdb00d94dabb9ce8089811cbf191d33
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-09 10:07:50 +0200

    add

---
+++ 003.ConditionalStatementsLab/004.PasswordGuess/PasswordGuess.cs
@@ -0,0 +1,20 @@
+﻿//Conditional statements - lab, Task 004
+//07.03.2023, 15:39
+using System;
+
+public class PasswordGuess
+{
+    static void Main()
+    {
+        string password = Console.ReadLine();
+
+        if(password == "s3cr3t!P@ssw0rd")
+        {
+            Console.WriteLine("Welcome");
+        }
+        else
+        {
+            Console.WriteLine("Wrong password!");
+        }
+    }
+}
