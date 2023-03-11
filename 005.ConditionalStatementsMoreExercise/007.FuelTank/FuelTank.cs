ef69e331346604923e6ecf11bf4a7ae548f5b70e
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-11 08:50:33 +0200

    add

---
+++ 005.ConditionalStatementsMoreExercise/007.FuelTank/FuelTank.cs
@@ -0,0 +1,51 @@
+﻿//Conditional statements - more exercise, Task 007
+//08.03.2023, 23:05
+using System;
+
+public class FuelTank
+{
+    static void Main()
+    {
+        string fuelType = Console.ReadLine().ToLower();
+        double litters = double.Parse(Console.ReadLine());
+
+        if (litters < 25)
+        {
+            if (fuelType == "diesel")
+            {
+                Console.WriteLine($"Fill your tank with {fuelType}!");
+            }
+            else if (fuelType == "gasoline")
+            {
+                Console.WriteLine($"Fill your tank with {fuelType}!");
+            }
+            else if (fuelType == "gas")
+            {
+                Console.WriteLine($"Fill your tank with {fuelType}!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid fuel!");
+            }
+        }
+        else if (litters >= 25)
+        {
+            if (fuelType == "diesel")
+            {
+                Console.WriteLine($"You have enough {fuelType}.");
+            }
+            else if (fuelType == "gasoline")
+            {
+                Console.WriteLine($"You have enough {fuelType}.");
+            }
+            else if (fuelType == "gas")
+            {
+                Console.WriteLine($"You have enough {fuelType}.");
+            }
+            else
+            {
+                Console.WriteLine($"Invalid fuel!");
+            }
+        }
+    }
+}
