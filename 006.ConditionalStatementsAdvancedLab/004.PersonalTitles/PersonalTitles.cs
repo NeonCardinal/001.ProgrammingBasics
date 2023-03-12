691d87783761d6c95bcb9523c6ae196683ffa2d3
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-12 13:01:49 +0200

    add

---
+++ 006.ConditionalStatementsAdvancedLab/004.PersonalTitles/PersonalTitles.cs
@@ -0,0 +1,35 @@
+﻿//Conditional statements advanced - lab, Task 004
+//09.03.2023, 10:49
+using System;
+
+public class PersonalTitles
+{
+    static void Main()
+    {
+        double age = double.Parse(Console.ReadLine());
+        string gender = Console.ReadLine();
+
+        if(gender == "m")
+        {
+            if(age < 16)
+            {
+                Console.WriteLine("Master");
+            }
+            else if(age >= 16)
+            {
+                Console.WriteLine("Mr.");
+            }
+        }
+        else if(gender == "f")
+        {
+            if(age < 16)
+            {
+                Console.WriteLine("Miss");
+            }
+            else if(age >= 16)
+            {
+                Console.WriteLine("Ms.");
+            }
+        }
+    }
+}
