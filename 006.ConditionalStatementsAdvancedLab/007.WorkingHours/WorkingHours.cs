371adbae7d2463c514fe766cf52276f0d4df9602
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-12 13:02:40 +0200

    add

---
+++ 006.ConditionalStatementsAdvancedLab/007.WorkingHours/WorkingHours.cs
@@ -0,0 +1,29 @@
+﻿//Conditional statements advanced - lab, Task 007
+//09.03.2023, 11:38
+using System;
+using System.Threading.Channels;
+
+public class WorkingHours
+{
+    static void Main()
+    {
+        int hour = int.Parse(Console.ReadLine());
+        string day = Console.ReadLine();
+
+        if(day == "Sunday")
+        {
+            Console.WriteLine("closed");
+        }
+        else
+        {
+            if(hour >= 10 && hour <= 18)
+            {
+                Console.WriteLine("open");
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}
