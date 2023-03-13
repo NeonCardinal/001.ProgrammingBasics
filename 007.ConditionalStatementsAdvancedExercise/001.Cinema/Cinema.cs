ad1201d4666a34a41c047c19a447def7fcdfe7f9
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-13 09:14:10 +0200

    add

---
+++ 007.ConditionalStatementsAdvancedExercise/001.Cinema/Cinema.cs
@@ -0,0 +1,29 @@
+﻿//Conditional statements advanced - exercise, Task 001
+//09.03.2023, 13:40
+using System;
+
+public class Cinema
+{
+    static void Main()
+    {
+        string type = Console.ReadLine();
+        int row = int.Parse(Console.ReadLine());
+        int col = int.Parse(Console.ReadLine());
+        double income = 0.0;
+
+        if(type == "Premiere")
+        {
+            income = (row * col) * 12.00;
+        }
+        else if(type == "Normal")
+        {
+            income = (row * col) * 7.50;
+        }
+        else if(type == "Discount")
+        {
+            income = (row * col) * 5.00;
+        }
+
+        Console.WriteLine($"{income:F2} leva");
+    }
+}
