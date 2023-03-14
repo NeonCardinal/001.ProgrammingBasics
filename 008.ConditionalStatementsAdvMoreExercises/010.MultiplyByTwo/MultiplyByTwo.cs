0e6baf3b900cb205351b0b5a50e883ebb3db37e7
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-14 09:23:14 +0200

    add

---
+++ 008.ConditionalStatementsAdvMoreExercises/010.MultiplyByTwo/MultiplyByTwo.cs
@@ -0,0 +1,24 @@
+﻿//Conditional statements advanced - more exercise, Task 010
+//10.03.2023, 13:53
+using System;
+
+public class MultiplyByTwo
+{
+    static void Main()
+    {
+        while (true)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            if (number < 0)
+            {
+                Console.WriteLine("Negative number!");
+                break;
+            }
+
+            double result = number * 2;
+
+            Console.WriteLine($"Result: {result:F2}");
+        }
+    }
+}
