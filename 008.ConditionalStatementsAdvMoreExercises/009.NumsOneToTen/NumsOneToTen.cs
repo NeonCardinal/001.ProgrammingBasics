c1b6bf069f616f873983f4b84424ddfc022e301c
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-14 09:22:56 +0200

    add

---
+++ 008.ConditionalStatementsAdvMoreExercises/009.NumsOneToTen/NumsOneToTen.cs
@@ -0,0 +1,17 @@
+﻿//Conditional statements advanced - more exercise, Task 009
+//10.03.2023, 13:51
+using System;
+
+public class NumsOneToTen
+{
+    static void Main()
+    {
+        int i = 1;
+
+        while(i <= 10)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+    }
+}
