c8c4bb6b109b8c63b6941010bc7e460ff40090f2
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-14 09:21:31 +0200

    add

---
+++ 008.ConditionalStatementsAdvMoreExercises/004.CarToGo/CarToGo.cs
@@ -0,0 +1,56 @@
+﻿//Conditional statements advanced - more exercise, Task 004
+//10.03.2023, 10:44
+using System;
+
+public class CarToGo
+{
+    static void Main()
+    {
+        double budjet = double.Parse(Console.ReadLine());
+        string season = Console.ReadLine();
+
+        double price = 0.00;
+        string carClass = string.Empty;
+        string carType = string.Empty;
+
+        if(budjet <= 100)
+        {
+            carClass = "Economy class";
+
+            if(season == "Summer")
+            {
+                carType = "Cabrio";
+                price = budjet * 0.35;
+            }
+            else if(season == "Winter")
+            {
+                carType = "Jeep";
+                price = budjet * 0.65;
+            }
+        }
+        else if(budjet > 100 && budjet <= 500)
+        {
+            carClass = "Compact class";
+
+            if(season == "Summer")
+            {
+                carType = "Cabrio";
+                price = budjet * 0.45;
+            }
+            else if(season == "Winter")
+            {
+                carType = "Jeep";
+                price = budjet * 0.80;
+            }
+        }
+        else if(budjet > 500)
+        {
+            carClass = "Luxury class";
+            carType = "Jeep";
+            price = budjet * 0.90;
+        }
+
+        Console.WriteLine(carClass);
+        Console.WriteLine($"{carType} - {price:F2}");
+    }
+}
