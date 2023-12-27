79e340b0a8944b5758d2ff658656b6d46c117e6f
NeonCardinal <nvnnaidenov@gmail.com>
2023-12-27 13:06:11 +0200

    add

---
+++ 001.FirstStepsInProgramming/002.Expression/Expression.cs
@@ -0,0 +1,12 @@
+﻿//Chapter First steps in programming, Task 002
+//24.12.2023, 19:58
+using System;
+
+
+public class Expression
+{
+    static void Main()
+    {
+        Console.WriteLine((3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3);
+    }
+}
