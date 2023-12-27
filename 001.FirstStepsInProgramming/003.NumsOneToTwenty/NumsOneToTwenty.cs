8a56dc9f2ac99c6179fcedb1682d540a8ad95ce4
NeonCardinal <nvnnaidenov@gmail.com>
2023-12-27 13:06:29 +0200

    add

---
+++ 001.FirstStepsInProgramming/003.NumsOneToTwenty/NumsOneToTwenty.cs
@@ -0,0 +1,14 @@
+﻿//Chapter First steps in programming, Task 003,
+//24.12.2023, 20:00
+using System;
+
+public class NumsOneToTwenty
+{
+    static void Main()
+    {
+        for(int i = 1; i <= 20; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
