ec93029ce991fb8545e8601bee68cd22e3fadcae
NeonCardinal <nvnnaidenov@gmail.com>
2023-12-27 15:19:49 +0200

    add

---
+++ 001.FirstStepsInProgramming/007.Sumator/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _007.Sumator
+{
+    public partial class SumatorForm : Form
+    {
+        public SumatorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal a = decimal.Parse(textBox1.Text);
+                decimal b = decimal.Parse(textBox2.Text);
+                decimal result = a + b;
+                textBoxSum.Text = result.ToString();
+            }
+            catch
+            {
+                textBoxSum.Text = "Error";
+            }
+        }
+    }
+}
