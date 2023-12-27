35477b3523e83f9ab98f30c22845baafbe52e2c0
NeonCardinal <nvnnaidenov@gmail.com>
2023-12-27 13:05:47 +0200

    add

---
+++ 001.FirstStepsInProgramming/001.HelloCSharp/HelloCSharp.cs
@@ -0,0 +1,11 @@
+﻿//Chapter First steps in programming, Task 001
+//24.12.2023, 19:56
+using System;
+
+public class HelloCSharp
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello C#");
+    }
+}
