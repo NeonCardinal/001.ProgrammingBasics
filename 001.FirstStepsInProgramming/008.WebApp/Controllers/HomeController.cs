fbc37ebaabd140be81008aa32a4869bd59132de1
NeonCardinal <nvnnaidenov@gmail.com>
2023-12-27 15:20:51 +0200

    add

---
+++ 001.FirstStepsInProgramming/008.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using _008.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _008.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public ActionResult Calculate(int num1, int num2)
+        {
+            this.ViewBag.num1 = num1;
+            this.ViewBag.num2 = num2;
+            this.ViewBag.sum = num1 + num2;
+
+            return View("Index");
+        }
+    }
+}
