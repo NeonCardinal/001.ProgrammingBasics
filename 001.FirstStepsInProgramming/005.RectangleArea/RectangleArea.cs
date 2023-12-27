52dbc62df7ab8bf6eb2caae09f741495e24c977e
NeonCardinal <nvnnaidenov@gmail.com>
2023-12-27 13:07:10 +0200

    add

---
+++ 001.FirstStepsInProgramming/005.RectangleArea/RectangleArea.cs
@@ -0,0 +1,16 @@
+﻿//Chapter First steps in programming, Task 005
+//24.12.2023, 20:05
+using System;
+
+public class RectangleArea
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        int area = a * b;
+
+        Console.WriteLine(area);
+    }
+}
