e0cfee2eb421e6bfac3a579c80d0559036c8b3d1
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-03 10:40:18 +0200

    add

---
+++ 002.SimpleCalculationsLab/005.TrapeziodArea/TrapeziodArea.cs
@@ -0,0 +1,17 @@
+﻿//Chapter Simple calculations - lab, Task 005
+//27.12.2023, 17:15
+using System;
+
+public class TrapeziodArea
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double h = double.Parse(Console.ReadLine());
+
+        double area = (a + b) * h / 2;
+
+        Console.WriteLine(area);
+    }
+}
