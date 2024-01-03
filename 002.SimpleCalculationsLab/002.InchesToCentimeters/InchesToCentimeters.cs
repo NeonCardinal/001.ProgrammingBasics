db539daa1307e0a90ce56cdb33a51b9f916755fb
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-03 10:39:23 +0200

    add

---
+++ 002.SimpleCalculationsLab/002.InchesToCentimeters/InchesToCentimeters.cs
@@ -0,0 +1,15 @@
+﻿//Chapter Simple calculations - lab, Task 002
+//27.12.2023, 15:45
+using System;
+
+public class InchesToCentimeters
+{
+    static void Main()
+    {
+        double inches = double.Parse(Console.ReadLine());
+
+        double centimeters = inches * 2.54;
+
+        Console.WriteLine(centimeters);
+    }
+}
