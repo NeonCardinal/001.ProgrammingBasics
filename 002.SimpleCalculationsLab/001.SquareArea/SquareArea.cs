9c53f2174c792b6daf4ba1e5b0de29a9cbae4e63
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-03 10:39:05 +0200

    add

---
+++ 002.SimpleCalculationsLab/001.SquareArea/SquareArea.cs
@@ -0,0 +1,15 @@
+﻿//Chapter Simple calculation - lab, Task 001
+//27.12.2023, 15:41
+using System;
+
+public class SquareArea
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+
+        int area = a * a;
+
+        Console.WriteLine(area);
+    }
+}
