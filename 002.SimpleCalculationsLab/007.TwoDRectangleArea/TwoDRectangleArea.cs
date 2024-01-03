66f582f5574a3f6bad9cc6a889396bd22dd0dd34
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-03 10:40:53 +0200

    add

---
+++ 002.SimpleCalculationsLab/007.TwoDRectangleArea/TwoDRectangleArea.cs
@@ -0,0 +1,22 @@
+﻿//Chapter Simple calculations - lab, Task 007
+//27.12.2023, 17:39
+using System;
+
+public class TwoDRectangleArea
+{
+    static void Main()
+    {
+        double x1 = double.Parse(Console.ReadLine());
+        double y1 = double.Parse(Console.ReadLine());
+        double x2 = double.Parse(Console.ReadLine());
+        double y2 = double.Parse(Console.ReadLine());
+
+        double width = Math.Abs(x1 - x2);
+        double height = Math.Abs(y1 - y2);
+        double area = width * height;
+        double perimeter = 2 * (width + height);
+
+        Console.WriteLine(area);
+        Console.WriteLine(perimeter);
+    }
+}
