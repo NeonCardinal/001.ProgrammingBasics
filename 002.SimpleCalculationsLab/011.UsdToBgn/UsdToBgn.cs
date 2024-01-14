3f7c61fb8a435d746de68e7057befa9aa0f2ed12
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-14 22:57:38 +0200

    add

---
+++ 002.SimpleCalculationsLab/011.UsdToBgn/UsdToBgn.cs
@@ -0,0 +1,15 @@
+﻿//Simple calculations - lab, Task 011,
+//11.01.2023, 20:55
+using System;
+
+public class UsdToBgn
+{
+    static void Main()
+    {
+        double usd = double.Parse(Console.ReadLine());
+
+        double leva = usd * 1.79549;
+
+        Console.WriteLine($"{leva:F2} BGN");
+    }
+}
