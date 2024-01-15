8e293bcf7b33ab0d97d48c510587ddf386e1dc67
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-15 18:12:36 +0200

    add

---
+++ 003.SimpleCalculationsExercise/004.Money/Money.cs
@@ -0,0 +1,23 @@
+﻿//Simple calculations - exercise, Task 004
+//12.01.2024, 00:26
+using System;
+
+public class Money
+{
+    static void Main()
+    {
+        int bitcoins = int.Parse(Console.ReadLine());
+        decimal yoani = decimal.Parse(Console.ReadLine());
+        decimal commision = decimal.Parse(Console.ReadLine()) / 100.0m;
+
+        decimal leva = bitcoins * 1168m;
+        decimal dollars = yoani * 0.15m;
+        //decimal dollarsToLeva = dollars * 1.76m;
+        leva += (dollars * 1.76m);
+        decimal euro = leva / 1.95m;
+        decimal comm = euro * commision;
+        decimal total = euro - comm;
+
+        Console.WriteLine(total);
+    }
+}
