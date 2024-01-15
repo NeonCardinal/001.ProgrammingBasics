1c32505db22460d80b5cd64b446c089fc25562ec
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-15 18:12:02 +0200

    add

---
+++ 003.SimpleCalculationsExercise/002.VegetableMarket/VegetableMarket.cs
@@ -0,0 +1,18 @@
+﻿//Simple calculations - exercise, Task 002
+//11.01.2024, 22:07
+using System;
+
+public class VegetableMarket
+{
+    static void Main()
+    {
+        double vegetablePrice = double.Parse(Console.ReadLine());
+        double fruitPrice = double.Parse(Console.ReadLine());
+        int vegetableWeight = int.Parse(Console.ReadLine());
+        int fruitWeight = int.Parse(Console.ReadLine());
+
+        double total = (vegetablePrice * vegetableWeight + fruitPrice * fruitWeight) / 1.94;
+
+        Console.WriteLine(total);
+    }
+}
