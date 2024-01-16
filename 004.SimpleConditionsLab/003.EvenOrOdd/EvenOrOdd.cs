a27726c1e4f375283680a224ba56e55018ae8ece
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-16 23:24:11 +0200

    add

---
+++ 004.SimpleConditionsLab/003.EvenOrOdd/EvenOrOdd.cs
@@ -0,0 +1,20 @@
+﻿//Simple conditions - lab, Task 003
+//12.01.2024, 01:34
+using System;
+
+public class EvenOrOdd
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        if(n % 2 == 0)
+        {
+            Console.WriteLine("even");
+        }
+        else
+        {
+            Console.WriteLine("odd");
+        }
+    }
+}
