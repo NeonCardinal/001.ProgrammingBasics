ca7a334cab12b807472819b78c3290e5a6b9cdc5
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-16 23:25:22 +0200

    add

---
+++ 004.SimpleConditionsLab/007.SumSeconds/SumSeconds.cs
@@ -0,0 +1,36 @@
+﻿//Simple conditions - lab, Task 007
+//12.01.2024, 23:50
+using System;
+
+public class SumSeconds
+{
+    static void Main()
+    {
+        int firstCompetitor = int.Parse(Console.ReadLine());
+        int secondCompetitor = int.Parse(Console.ReadLine());
+        int thirdCompetitor = int.Parse(Console.ReadLine());
+
+        int minutes = 0;
+        int seconds = firstCompetitor + secondCompetitor + thirdCompetitor;
+
+        if(seconds >= 60)
+        {
+            minutes++;
+            seconds -= 60;
+        }
+        if(seconds >= 60)
+        {
+            minutes++;
+            seconds -= 60;
+        }
+
+        if(seconds < 10)
+        {
+            Console.WriteLine($"{minutes}:0{seconds}");
+        }
+        else
+        {
+            Console.WriteLine($"{minutes}:{seconds}");
+        }
+    }
+}
