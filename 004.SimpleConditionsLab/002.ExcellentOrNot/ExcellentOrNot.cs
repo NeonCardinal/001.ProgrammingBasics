f2724fb7ef1a98de121cea5d29b0f0cd0eb2d303
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-16 23:23:54 +0200

    add

---
+++ 004.SimpleConditionsLab/002.ExcellentOrNot/ExcellentOrNot.cs
@@ -0,0 +1,20 @@
+﻿//Simple conditions - lab, Task 002
+//12.01.2024, 01:31
+using System;
+
+public class ExcellentOrNot
+{
+    static void Main()
+    {
+        double grade = double.Parse(Console.ReadLine());
+
+        if(grade >= 5.50)
+        {
+            Console.WriteLine("Excellent!");
+        }
+        else
+        {
+            Console.WriteLine("Not excellent.");
+        }
+    }
+}
