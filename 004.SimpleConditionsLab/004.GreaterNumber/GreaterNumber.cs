b99c9e73b1b78dd72e69ee0013d77d77c3852fe8
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-16 23:24:29 +0200

    add

---
+++ 004.SimpleConditionsLab/004.GreaterNumber/GreaterNumber.cs
@@ -0,0 +1,21 @@
+﻿//Simple conditions - lab, Task 004
+//12.01.2024, 01:37
+using System;
+
+public class GreaterNumber
+{
+    static void Main()
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+
+        if(firstNumber > secondNumber)
+        {
+            Console.WriteLine(firstNumber);
+        }
+        else
+        {
+            Console.WriteLine(secondNumber);
+        }
+    }
+}
