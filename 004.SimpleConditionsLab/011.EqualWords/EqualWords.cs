7d90b411f325c7b680f5c8855cb9619e20521518
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-17 15:29:25 +0200

    add

---
+++ 004.SimpleConditionsLab/011.EqualWords/EqualWords.cs
@@ -0,0 +1,21 @@
+﻿//Conditional statements - lab, Task 011
+//13.01.2024, 00:40
+using System;
+
+public class EqualWords
+{
+    static void Main()
+    {
+        string firstWord = Console.ReadLine().ToLower();
+        string secondWord = Console.ReadLine().ToLower();
+
+        if(firstWord == secondWord)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
