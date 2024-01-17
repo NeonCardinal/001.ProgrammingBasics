49fb4aa96d59b59d00c8ece6085e383f5cca870e
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-17 15:28:47 +0200

    add

---
+++ 004.SimpleConditionsLab/009.PasswordGuess/PasswordGuess.cs
@@ -0,0 +1,21 @@
+﻿//Simple conditions - lab, Task 009
+//13.01.2024, 00:08
+using System;
+
+public class PasswordGuess
+{
+    static void Main()
+    {
+        string pass = Console.ReadLine();
+        string password = "s3cr3t!P@ssw0rd";
+
+        if(pass == password)
+        {
+            Console.WriteLine("Welcome");
+        }
+        else
+        {
+            Console.WriteLine("Wrong password!");
+        }
+    }
+}
