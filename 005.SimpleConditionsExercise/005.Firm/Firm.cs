fa50ec4560b3d384e9ec9339ae4275ebd6e6e22f
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-18 20:54:58 +0200

    add

---
+++ 005.SimpleConditionsExercise/005.Firm/Firm.cs
@@ -0,0 +1,26 @@
+﻿//Simple conditions - exercise, Task 005
+//15.01.2024, 19:50
+using System;
+
+public class Firm
+{
+    static void Main()
+    {
+        int projectHours = int.Parse(Console.ReadLine());
+        int availableDays = int.Parse(Console.ReadLine());
+        int overtimeWorkers = int.Parse(Console.ReadLine());
+
+        var workDays = availableDays * 0.90;
+        var overtime = overtimeWorkers * 2 * availableDays;
+        var workHours = Math.Floor(workDays * 8 + overtime);
+
+        if (workHours < projectHours)
+        {
+            Console.WriteLine($"Yes!{projectHours - workHours} hours left.");
+        }
+        else
+        {
+            Console.WriteLine($"Not enough time!{Math.Abs(projectHours - workHours)} hours needed.");
+        }
+    }
+}
