249622093cfe268a972a3176f90ebaadc70b4258
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-19 17:15:51 +0200

    add

---
+++ 006.ComplexConditionsLab/004.FruitOrVegetable/FruitOrVegetable.cs
@@ -0,0 +1,25 @@
+﻿//Complex conditions - lab, Task 004
+//17.01.2024, 20:48
+using System;
+
+public class FruitOrVegetable
+{
+    static void Main()
+    {
+        string str = Console.ReadLine();
+
+        if(str == "banana" || str == "apple" || str == "kiwi" || str == "cherry" || str == "lemon"
+            || str == "grapes")
+        {
+            Console.WriteLine("fruit");
+        }
+        else if(str == "tomato" || str == "cucumber" || str == "pepper" || str == "carrot")
+        {
+            Console.WriteLine("vegetable");
+        }
+        else
+        {
+            Console.WriteLine("unknown");
+        }
+    }
+}
