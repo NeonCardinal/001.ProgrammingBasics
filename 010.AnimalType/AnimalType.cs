0a11a0e33b4923e8b0da7643776848d5d10798ac
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-20 20:56:49 +0200

    add

---
+++ 010.AnimalType/AnimalType.cs
@@ -0,0 +1,26 @@
+﻿//Complex conditions - lab, Task 010
+//17.01.2024, 22:04
+using System;
+
+public class AnimalType
+{
+    static void Main()
+    {
+        string animal = Console.ReadLine();
+
+        switch(animal)
+        {
+            case "dog":
+                Console.WriteLine("mammal");
+                break;
+            case "crocodile":
+            case "tortoise":
+            case "snake":
+                Console.WriteLine("reptile");
+                break;
+            default:
+                Console.WriteLine("unknown");
+                break;
+        }
+    }
+}
