de712b1ea421ae7c6e705c2396518f638857d657
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-21 21:04:47 +0200

    add

---
+++ 006.ComplexConditionsLab/012.Volleyball/Volleyball.cs
@@ -0,0 +1,27 @@
+﻿//Complex conditions - lab, Task 012
+//17.01.2024, 22:59
+using System;
+
+public class Volleyball
+{
+    static void Main()
+    {
+        string yearType = Console.ReadLine();
+        int holidays = int.Parse(Console.ReadLine());
+        int weenedsInHometown = int.Parse(Console.ReadLine());
+
+        double sofiaWeekends = (48 - weenedsInHometown) * 3.0 / 4;
+        double gamesHolidays = holidays * 2.0 / 3;
+        double totalGames = sofiaWeekends + weenedsInHometown + gamesHolidays;
+
+        if (yearType == "normal")
+        {
+            Console.WriteLine(Math.Truncate(totalGames));
+        }
+        else if (yearType == "leap")
+        {
+            double LeapYear = totalGames * 15 / 100;
+            Console.WriteLine(Math.Truncate(totalGames + LeapYear));
+        }
+    }
+}
