6d351ce25d127f8d041f8194da988108ff31cbc2
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-23 19:06:03 +0200

    add

---
+++ 008.LoopsLab/003.LatinLetters/LatinLetters.cs
@@ -0,0 +1,14 @@
+﻿//Loops - lab, Task 003
+//21.01.2024, 21:46
+using System;
+
+public class LatinLetters
+{
+    static void Main()
+    {
+        for(char ch = 'a'; ch <= 'z'; ch++)
+        {
+            Console.WriteLine(" " + ch);
+        }
+    }
+}
