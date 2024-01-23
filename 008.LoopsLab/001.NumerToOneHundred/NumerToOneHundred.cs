7ecb7380691b9ad1741dc6b49436cbac26734ea3
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-23 19:05:30 +0200

    add

---
+++ 008.LoopsLab/001.NumerToOneHundred/NumerToOneHundred.cs
@@ -0,0 +1,14 @@
+﻿//Loops - lab, Task 001
+//21.01.2024, 21:16
+using System;
+
+public class NumerToOneHundred
+{
+    static void Main()
+    {
+        for(int i = 1; i <= 100; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
