3a55aaa0b09fbc31e32e68cc6d43a096bee20de5
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-23 19:06:38 +0200

    add

---
+++ 008.LoopsLab/005.MaxNumber/MaxNumber.cs
@@ -0,0 +1,25 @@
+﻿//Loops - lab, Task 005
+//21.01.2024, 21:53
+using System;
+
+public class MaxNumber
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int max = int.MinValue;
+
+        for(int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if(number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine(max);
+    }
+}
