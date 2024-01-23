3f88f9e5e6856dbaf42ccbf99ff05633b027a8ee
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-23 19:06:20 +0200

    add

---
+++ 008.LoopsLab/004.SumNumbers/SumNumbers.cs
@@ -0,0 +1,22 @@
+﻿//Loops - lab, Task 004
+//21.01.2024, 21:49
+using System;
+
+public class SumNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            sum += number;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
