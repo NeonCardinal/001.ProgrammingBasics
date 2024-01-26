d06f538bc748c3f94665ce2c5f07c1cf825c53b4
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-26 18:19:38 +0200

    add

---
+++ 010.NestedLoopsLab/003.SquareOfStars/SquareOfStars.cs
@@ -0,0 +1,21 @@
+﻿//Nested loops - lab, Task 003
+//23.01.2024, 20:33
+using System;
+
+public class SquareOfStars
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
