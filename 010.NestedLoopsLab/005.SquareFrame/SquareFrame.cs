01ec407786a5745d71f9f291c12c7d0584c71cfc
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-26 18:20:17 +0200

    add

---
+++ 010.NestedLoopsLab/005.SquareFrame/SquareFrame.cs
@@ -0,0 +1,35 @@
+﻿//Nested loops - lab, Task 005
+//23.01.2024, 20:40
+using System;
+
+public class SquareFrame
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("+");
+        for(int i = 0; i < n -2; i++)
+        {
+            Console.Write("-");
+        }
+        Console.WriteLine("+");
+
+        for(int i = 0; i < n - 2; i++)
+        {
+            Console.Write("|");
+            for (int j = 0; j < n - 2; j++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("|");
+        }
+
+        Console.Write("+");
+        for (int i = 0; i < n - 2; i++)
+        {
+            Console.Write("-");
+        }
+        Console.WriteLine("+");
+    }
+}
