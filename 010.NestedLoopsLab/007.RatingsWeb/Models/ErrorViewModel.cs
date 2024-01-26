e0e6e4145b4254e6a122df41408f940609875c85
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-26 18:21:11 +0200

    add -> need fix

---
+++ 010.NestedLoopsLab/007.RatingsWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _007.RatingsWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
