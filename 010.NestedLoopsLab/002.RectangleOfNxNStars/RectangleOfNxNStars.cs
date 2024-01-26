3bcb3909d78f4b48ccd0a28fde59b3d28e65265e
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-26 18:19:18 +0200

    add

---
+++ 010.NestedLoopsLab/002.RectangleOfNxNStars/RectangleOfNxNStars.cs
@@ -0,0 +1,16 @@
+﻿//Nested loops - exercise, Task 002
+//23.01.2024, 20:30
+using System;
+
+public class RectangleOfNxNStars
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 0; i < n; i++)
+        {
+            Console.WriteLine(new string('*', n));
+        }
+    }
+}
