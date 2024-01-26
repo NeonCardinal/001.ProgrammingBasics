e622f62f18be74a645257fac765aadb324974eba
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-26 18:18:56 +0200

    add

---
+++ 010.NestedLoopsLab/001.RectangleOfTenXTenStars/RectangleOfTenXTenStars.cs
@@ -0,0 +1,14 @@
+﻿//Nested loops - lab, Task 001
+//23.01.2024, 20:28
+using System;
+
+public class RectangleOfTenXTenStars
+{
+    static void Main()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(new string('*', 10));
+        }
+    }
+}
