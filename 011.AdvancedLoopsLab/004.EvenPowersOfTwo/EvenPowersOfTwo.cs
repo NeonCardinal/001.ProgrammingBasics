5c94d32a40b0bacf7d0f2bbf5206f1716b0b5131
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-27 11:31:32 +0200

    add

---
+++ 011.AdvancedLoopsLab/004.EvenPowersOfTwo/EvenPowersOfTwo.cs
@@ -0,0 +1,19 @@
+﻿//Advanced loops - lab, Task 004
+//24.01.2024, 00:07
+using System;
+
+public class EvenPowersOfTwo
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int num = 1;
+
+        for(int i = 0; i <= n; i += 2)
+        {
+            Console.WriteLine(num);
+            num = num * 2 * 2;
+        }
+    }
+}
