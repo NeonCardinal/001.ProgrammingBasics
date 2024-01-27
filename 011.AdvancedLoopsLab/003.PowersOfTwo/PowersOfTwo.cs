22c5d72e1c2d811f740373ad11ac7ed5ee027c10
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-27 11:31:13 +0200

    add

---
+++ 011.AdvancedLoopsLab/003.PowersOfTwo/PowersOfTwo.cs
@@ -0,0 +1,19 @@
+﻿//Advanced loops - lab, Task 003
+//23.01.2024, 22:29
+using System;
+
+public class PowersOfTwo
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int num = 1;
+
+        for(int i = 0; i <= n; i++)
+        {
+            Console.WriteLine(num);
+            num = num * 2;
+        }
+    }
+}
