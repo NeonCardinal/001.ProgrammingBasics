5cecd8479b623d83a702333006cec8193520e703
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-27 11:32:49 +0200

    add

---
+++ 011.AdvancedLoopsLab/007.GreatestCommonDivisor/GreatestCommonDivisor.cs
@@ -0,0 +1,21 @@
+﻿//Advanced loops - lab, Task 007
+//24.01.2024, 00:18
+using System;
+
+public class GreatestCommonDivisor
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        while(b != 0)
+        {
+            var oldB = b;
+            b = a % b;
+            a = oldB;
+        }
+
+        Console.WriteLine($"GCD = {a}");
+    }
+}
