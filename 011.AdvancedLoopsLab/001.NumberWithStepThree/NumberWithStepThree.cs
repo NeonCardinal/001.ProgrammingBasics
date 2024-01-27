abfbf257413b4eb30eaa7b7995323aae19787eca
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-27 11:30:23 +0200

    add

---
+++ 011.AdvancedLoopsLab/001.NumberWithStepThree/NumberWithStepThree.cs
@@ -0,0 +1,16 @@
+﻿//Advanced loops - lab, Task 001
+//23.01.2024, 22:25
+using System;
+
+public class NumberWithStepThree
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i <= n; i += 3)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
