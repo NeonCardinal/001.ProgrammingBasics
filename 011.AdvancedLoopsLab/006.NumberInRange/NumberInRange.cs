c70ced9cc01ba8e1f739166469dfb6b953c25508
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-27 11:32:14 +0200

    add

---
+++ 011.AdvancedLoopsLab/006.NumberInRange/NumberInRange.cs
@@ -0,0 +1,19 @@
+﻿//Advanced loops - lab, Task 006
+//24.01.2024, 00:14
+using System;
+
+public class NumberInRange
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        while(number < 1 || number > 100)
+        {
+            Console.WriteLine("Ivalid number!");
+            number = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine(number);
+    }
+}
