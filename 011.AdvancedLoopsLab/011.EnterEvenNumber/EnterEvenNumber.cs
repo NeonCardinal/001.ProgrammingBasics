a2bb5e87e63490bfaaa44478bace61a81589a38b
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-28 22:47:24 +0200

    add

---
+++ 011.AdvancedLoopsLab/011.EnterEvenNumber/EnterEvenNumber.cs
@@ -0,0 +1,26 @@
+﻿//Advanced loops - lab, Task 011
+//24.01.2024, 00:49
+using System;
+
+public class EnterEvenNumber
+{
+    static void Main()
+    {
+        int n = 0;
+
+        while(true)
+        {
+            Console.Write("Enter even number: ");
+            n = int.Parse(Console.ReadLine());
+            
+            if(n % 2 == 0)
+            {
+                break;
+            }
+
+            Console.WriteLine("The number is not even.");
+        }
+
+        Console.WriteLine(n);
+    }
+}
