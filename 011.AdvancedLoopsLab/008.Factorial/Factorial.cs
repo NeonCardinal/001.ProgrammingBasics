757f0fe8e87b843b0034ce5f096b74ad33fb2bcb
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-28 22:46:27 +0200

    add

---
+++ 011.AdvancedLoopsLab/008.Factorial/Factorial.cs
@@ -0,0 +1,22 @@
+﻿//Advanced loops - lab, Task 008
+//24.01.2024, 00:37
+using System;
+
+public class Factorial
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int fact = 1;
+
+        do
+        {
+            fact *= n;
+            n--;
+        }
+        while (n > 1);
+
+        Console.WriteLine(fact);
+    }
+}
