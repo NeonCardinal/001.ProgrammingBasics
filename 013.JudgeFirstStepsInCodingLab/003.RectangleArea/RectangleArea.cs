6fc3949c8f157c0a1025f3112e03198bc63bb16f
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-04 23:23:09 +0200

    add

---
+++ 013.JudgeFirstStepsInCodingLab/003.RectangleArea/RectangleArea.cs
@@ -0,0 +1,9 @@
+﻿//Judge: First steps in coding - lab
+//04.03.2024, 22:43
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+int area = a * b;
+
+Console.WriteLine(area);
