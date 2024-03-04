2f4e42cb152fd4693eea56673b2b6b519f3de261
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-05 00:14:36 +0200

    add

---
+++ 014.JudgeFirstStepsInCodingExercise/009.FishTank/FishTank.cs
@@ -0,0 +1,13 @@
+﻿//Judge: First steps in coding - exercise, Task 009
+//05.03.2024, 00:00
+
+int len = int.Parse(Console.ReadLine());
+int widht = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+double percent = double.Parse(Console.ReadLine());
+
+double volume = (len * widht * height) * 0.001;
+double takenArea = volume * (percent / 100.0);
+double total = volume - takenArea;
+
+Console.WriteLine(total);
