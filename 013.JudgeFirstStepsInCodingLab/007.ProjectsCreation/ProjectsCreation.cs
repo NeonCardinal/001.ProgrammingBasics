7ca153a3c5bc6098c03d0f6b1a6c7428134e075c
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-04 23:24:17 +0200

    add

---
+++ 013.JudgeFirstStepsInCodingLab/007.ProjectsCreation/ProjectsCreation.cs
@@ -0,0 +1,9 @@
+﻿//Judge: First steps in coding - lab, Task 007
+//04.03.2024, 23:03
+
+string name = Console.ReadLine();
+int projectsCnt = int.Parse(Console.ReadLine());
+
+int neededHours = projectsCnt * 3;
+
+Console.WriteLine($"The architect {name} will need {neededHours} hours to complete {projectsCnt} project/s.");
