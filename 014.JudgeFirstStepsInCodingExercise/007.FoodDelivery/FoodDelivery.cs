a4eb0cab5398e27689c50f202db5a00c7f70abd4
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-05 00:13:59 +0200

    add

---
+++ 014.JudgeFirstStepsInCodingExercise/007.FoodDelivery/FoodDelivery.cs
@@ -0,0 +1,15 @@
+﻿//Judge: First steps in coding - exercise, Task 007
+//04.03.2024, 23:56
+
+int chicken = int.Parse(Console.ReadLine());
+int fish = int.Parse(Console.ReadLine());
+int vegetable = int.Parse(Console.ReadLine());
+
+double chickenPrice = chicken * 10.35;
+double fishPrice = fish * 12.40;
+double vegetablePrice = vegetable * 8.15;
+double foodPrice = chickenPrice + fishPrice + vegetablePrice;
+double desertPrice = foodPrice * 0.20;
+double total = foodPrice + desertPrice + 2.50;
+
+Console.WriteLine(total);
