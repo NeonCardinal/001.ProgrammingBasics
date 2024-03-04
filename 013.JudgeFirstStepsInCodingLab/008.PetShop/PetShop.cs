f0f089347c4c29f18f172a2b69a2cff84d85b479
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-04 23:24:33 +0200

    add

---
+++ 013.JudgeFirstStepsInCodingLab/008.PetShop/PetShop.cs
@@ -0,0 +1,9 @@
+﻿//Judge: First steps in coding - lab, Task 008
+//04.03.2024, 23:08
+
+int dogFoodCnt = int.Parse(Console.ReadLine());
+int catFoodCnt = int.Parse(Console.ReadLine());
+
+double total = dogFoodCnt * 2.50 + catFoodCnt * 4.00;
+
+Console.WriteLine($"{total} lv.");
