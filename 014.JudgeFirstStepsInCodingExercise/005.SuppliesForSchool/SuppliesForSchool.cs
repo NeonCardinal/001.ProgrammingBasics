674a98dd066b9964e6b295e8ddbbdf2ad7e5c12b
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-05 00:13:25 +0200

    add

---
+++ 014.JudgeFirstStepsInCodingExercise/005.SuppliesForSchool/SuppliesForSchool.cs
@@ -0,0 +1,17 @@
+﻿//Judge: First steps in coding - exercise, Task 005
+//04.03.2024, 23:47
+
+int pensCnt = int.Parse(Console.ReadLine());
+int markersCnt = int.Parse(Console.ReadLine());
+int litters = int.Parse(Console.ReadLine());
+int discountPercent = int.Parse(Console.ReadLine());
+
+double pensPrice = pensCnt * 5.80;
+double markersPrice = markersCnt * 7.20;
+double littersPrice = litters * 1.20;
+double total = pensPrice + markersPrice + littersPrice;
+double discount = total * (discountPercent / 100.00);
+total -= discount;
+
+Console.WriteLine(total);
+
