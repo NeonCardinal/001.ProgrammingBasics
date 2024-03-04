5f80581b542ebb2afd5815c65e2ca0cc5986c56e
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-05 00:12:50 +0200

    add

---
+++ 014.JudgeFirstStepsInCodingExercise/003.DepositCalculator/DepositCalculator.cs
@@ -0,0 +1,12 @@
+﻿//Judge: First steps in coding - exercise, Task 003
+//04.03.2024, 23:38
+
+double deposit = double.Parse(Console.ReadLine());
+int depositPeriod = int.Parse(Console.ReadLine());
+double addPercent = double.Parse(Console.ReadLine());
+
+double add = deposit * (addPercent / 100.0);
+double addPerMonth = add / 12;
+double total = deposit + depositPeriod * addPerMonth;
+
+Console.WriteLine(total);
