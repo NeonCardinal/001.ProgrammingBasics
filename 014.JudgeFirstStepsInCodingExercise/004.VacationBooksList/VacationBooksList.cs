d49ccc2b4e3f9f0acaeea07d9491ea0c307585af
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-05 00:13:07 +0200

    add

---
+++ 014.JudgeFirstStepsInCodingExercise/004.VacationBooksList/VacationBooksList.cs
@@ -0,0 +1,11 @@
+﻿//Judge: First steps in coding - exercise, Task 004
+//04.03.2024, 23:44
+
+int pagesCount = int.Parse(Console.ReadLine());
+int pagesPerHour = int.Parse(Console.ReadLine());
+int days = int.Parse(Console.ReadLine());
+
+int totalTimePerBook = pagesCount / pagesPerHour;
+int neededHours = totalTimePerBook / days;
+
+Console.WriteLine(neededHours);
