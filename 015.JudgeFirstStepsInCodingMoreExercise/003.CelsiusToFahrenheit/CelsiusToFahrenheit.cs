e53eb9cc8df7ae9e9967155cd8c735ca20402bba
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-06 00:49:43 +0200

    add

---
+++ 015.JudgeFirstStepsInCodingMoreExercise/003.CelsiusToFahrenheit/CelsiusToFahrenheit.cs
@@ -0,0 +1,8 @@
+﻿//Judge: First steps in coding - more exercise, Task 003
+//05.03.2024, 00:36
+
+double celsuis = double.Parse(Console.ReadLine());
+
+double fahrenheit = (celsuis * 9 / 5) + 32;
+
+Console.WriteLine($"{fahrenheit:F2}");
