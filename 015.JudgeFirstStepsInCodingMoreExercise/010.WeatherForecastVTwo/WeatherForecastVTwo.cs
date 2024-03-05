44ab1786efd561379cda53acd02b84d505664b05
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-06 00:52:35 +0200

    add

---
+++ 015.JudgeFirstStepsInCodingMoreExercise/010.WeatherForecastVTwo/WeatherForecastVTwo.cs
@@ -0,0 +1,29 @@
+﻿//Judge: First steps in coding - more exercise, Task 010
+//05.03.2024, 20:45
+
+double deg = double.Parse(Console.ReadLine());
+
+if(deg >= 25.00 && deg <= 35.00)
+{
+    Console.WriteLine("Hot");
+}
+else if(deg >= 20.1 && deg <= 25.9)
+{
+    Console.WriteLine("Warm");
+}
+else if(deg >= 15.00 && deg <= 20.00)
+{
+    Console.WriteLine("Mild");
+}
+else if(deg >= 12.00 && deg <= 14.9)
+{
+    Console.WriteLine("Cool");
+}
+else if(deg >= 5.00 & deg <= 11.9)
+{
+    Console.WriteLine("Cold");
+}
+else
+{
+    Console.WriteLine("unknown");
+}
