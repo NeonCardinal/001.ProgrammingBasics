e02c1e7b16f484b1867e0fedd7b9db152f5c140b
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-06 00:48:56 +0200

    add

---
+++ 015.JudgeFirstStepsInCodingMoreExercise/001.TrapeziodArea/TrapeziodArea.cs
@@ -0,0 +1,10 @@
+﻿//Judge: First steps in coding - more exercise, Task 001
+//05.03.2024, 00:23
+
+double b1 = double.Parse(Console.ReadLine());
+double b2 = double.Parse(Console.ReadLine());
+double h  = double.Parse(Console.ReadLine());
+
+double area = (b1 + b2) * h / 2;
+
+Console.WriteLine($"{area:F2}");
