14772901016ac32cb8c7f1f01df2e99379f31e0e
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-06 00:52:11 +0200

    add

---
+++ 015.JudgeFirstStepsInCodingMoreExercise/009.WeatherForecast/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿//Judge: First steps in coding - more exercise, Task 009
+//05.03.2024, 20:39
+
+string str = Console.ReadLine().ToLower();
+
+if(str == "sunny")
+{
+    Console.WriteLine("It's warm outside!");
+}
+else
+{
+    Console.WriteLine("It's cold outside!");
+}
