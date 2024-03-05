08984107a9bd60d31b5261720556eb268891a692
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-06 00:50:29 +0200

    add

---
+++ 015.JudgeFirstStepsInCodingMoreExercise/005.TrainingLab/TrainingLab.cs
@@ -0,0 +1,14 @@
+﻿//Judge: First steps in coding - more exercise, Task 005
+//05.03.2024, 20:27
+
+double w = double.Parse(Console.ReadLine());
+double h = double.Parse(Console.ReadLine());
+
+double widthInCm = w * 100;
+double heightInCm = (h * 100) - 100;
+
+int row = (int)(widthInCm / 120);
+int col = (int)(heightInCm / 70);
+int result = row * col - 3;
+
+Console.WriteLine(result);
