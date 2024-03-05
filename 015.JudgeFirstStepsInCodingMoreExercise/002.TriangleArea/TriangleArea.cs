f36c75e0a6a221996f742fa974b9f8f9a897e147
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-06 00:49:20 +0200

    add

---
+++ 015.JudgeFirstStepsInCodingMoreExercise/002.TriangleArea/TriangleArea.cs
@@ -0,0 +1,9 @@
+﻿//Judge: First steps in conding - more exercise, Task 002
+//05.03.2024, 00:30
+
+double a = double.Parse(Console.ReadLine());
+double h = double.Parse(Console.ReadLine());
+
+double area = a * h / 2;
+
+Console.WriteLine($"{area:F2}");
