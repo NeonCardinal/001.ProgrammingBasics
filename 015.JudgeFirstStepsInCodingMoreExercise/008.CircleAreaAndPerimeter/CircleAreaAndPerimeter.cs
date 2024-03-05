01e84122ee3af0f3217a149cd2e5067e7b5b51ec
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-06 00:51:43 +0200

    add

---
+++ 015.JudgeFirstStepsInCodingMoreExercise/008.CircleAreaAndPerimeter/CircleAreaAndPerimeter.cs
@@ -0,0 +1,10 @@
+﻿//Judge: First steps in coding - more exercise, Task 008
+//05.03.2024, 20:34
+
+double r = double.Parse(Console.ReadLine());
+
+double area = Math.PI * r * r;
+double perimeter = 2 * Math.PI * r;
+
+Console.WriteLine($"{area:F2}");
+Console.WriteLine($"{perimeter:F2}");
