0b5ddb22f7e3ae12c224caf938bdc1cca83e45d1
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-06 00:50:06 +0200

    add

---
+++ 015.JudgeFirstStepsInCodingMoreExercise/004.VegetableMarket/VegetableMarket.cs
@@ -0,0 +1,12 @@
+﻿//Judge: First steps in coding - exercise, Task 004
+//05.03.2024, 00:57
+
+double priceForVegetables = double.Parse(Console.ReadLine());
+double priceForFruits = double.Parse(Console.ReadLine());
+int vegetablesWeight = int.Parse(Console.ReadLine());
+int fruitsWeight = int.Parse(Console.ReadLine());
+
+double total = priceForVegetables * vegetablesWeight + priceForFruits *  fruitsWeight;
+total /= 1.94;
+
+Console.WriteLine($"{total:F2}");
