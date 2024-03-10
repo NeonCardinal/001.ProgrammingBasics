8d8830d81bd6f67ba396db46f7bebf86e7bddfc3
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-10 22:54:43 +0200

    ADD

---
+++ 016.JudgeConditionalStatementsLab/001.ExcellentResult/ExcellentResult.cs
@@ -0,0 +1,9 @@
+﻿//Judge: Conditional statements - lab, Task 001
+//05.03.2024, 21:21
+
+double grade = double.Parse(Console.ReadLine());
+
+if(grade >= 5.50)
+{
+    Console.WriteLine("Excellent!");
+}
