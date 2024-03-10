83e5a3a8dd76a7da61468b1405a4ef7fa6a970ce
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-10 22:55:23 +0200

    add

---
+++ 016.JudgeConditionalStatementsLab/003.EvenOrOdd/EvenOrOdd.cs
@@ -0,0 +1,13 @@
+﻿//Judge: Conditional statements - lab, Task 003
+//05.03.2024, 21:25
+
+int number = int.Parse(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    Console.WriteLine("even");
+}
+else
+{
+    Console.WriteLine("odd");
+}
