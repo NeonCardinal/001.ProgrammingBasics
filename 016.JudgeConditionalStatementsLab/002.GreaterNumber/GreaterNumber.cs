825c92f0a26489557f5c48efdf3c9d1d866cde54
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-10 22:55:00 +0200

    add

---
+++ 016.JudgeConditionalStatementsLab/002.GreaterNumber/GreaterNumber.cs
@@ -0,0 +1,14 @@
+﻿//Judge: Conditional statements - lab, Task 002
+//05.03.2024, 21:23
+
+int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+
+if(firstNumber > secondNumber)
+{
+    Console.WriteLine(firstNumber);
+}
+else
+{
+    Console.WriteLine(secondNumber);
+}
