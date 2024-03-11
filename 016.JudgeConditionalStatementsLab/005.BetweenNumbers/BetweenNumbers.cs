0260f0e8b16c4a11ccffc09a4ffff29e8f2cec40
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-11 20:36:52 +0200

    add

---
+++ 016.JudgeConditionalStatementsLab/005.BetweenNumbers/BetweenNumbers.cs
@@ -0,0 +1,17 @@
+﻿//Judge: Conditional statements - lab, Task 005
+//05.03.2024, 21:30
+
+int number = int.Parse(Console.ReadLine());
+
+if(number < 100)
+{
+    Console.WriteLine("Less than 100");
+}
+else if(number >= 100 && number <= 200)
+{
+    Console.WriteLine("Between 100 and 200");
+}
+else if(number > 200)
+{
+    Console.WriteLine("Greater than 200");
+}
