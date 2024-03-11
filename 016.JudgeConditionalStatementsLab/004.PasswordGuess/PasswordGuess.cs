dcb637bd1e0e1e8aa70ec7257f14242bf91e01dd
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-11 20:36:34 +0200

    add

---
+++ 016.JudgeConditionalStatementsLab/004.PasswordGuess/PasswordGuess.cs
@@ -0,0 +1,13 @@
+﻿//Judge: Conditional statements - lab, Task 004
+//05.04.2024, 21:28
+
+string pass = Console.ReadLine();
+
+if(pass == "s3cr3t!P@ssw0rd")
+{
+    Console.WriteLine("Welcome");
+}
+else
+{
+    Console.WriteLine("Wrong password!");
+}
