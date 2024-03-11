b80f491b256e05c640e19e1548dbc8f522b8f873
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-11 20:37:30 +0200

    add

---
+++ 016.JudgeConditionalStatementsLab/007.AreaOfFigures/AreaOfFigures.cs
@@ -0,0 +1,39 @@
+﻿//Judge: Conditional statements - lab, Task 007
+//05.03.2024, 21:35
+
+string figure = Console.ReadLine();
+
+if (figure == "square")
+{
+    double a = double.Parse(Console.ReadLine());
+
+    double area = a * a;
+
+    Console.WriteLine($"{area:F3}");
+}
+else if (figure == "rectangle")
+{
+    double a = double.Parse(Console.ReadLine());
+    double b = double.Parse(Console.ReadLine());
+
+    double area = a * b;
+
+    Console.WriteLine($"{area:F3}");
+}
+else if (figure == "circle")
+{
+    double r = double.Parse(Console.ReadLine());
+
+    double area = Math.PI * r * r;
+
+    Console.WriteLine($"{area:F3}");
+}
+else if (figure == "triangle")
+{
+    double a = double.Parse(Console.ReadLine());
+    double h = double.Parse(Console.ReadLine());
+
+    double area = a * h / 2;
+
+    Console.WriteLine($"{area:F3}");
+}
